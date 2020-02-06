52ac86bc02cb93452fdfc24f302e07590e9790a9
Anton <antonhh@live.dk>
2020-02-06 15:47:16 +0100

    Initial Commit

---
+++ Source_Control_Manuel/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Source_Control_Manuel
+//
+//  Created by Anton on 06/02/2020.
+//  Copyright © 2020 Anton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
